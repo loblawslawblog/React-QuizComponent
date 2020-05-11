27f94bcad6c8ce811d719c6d5ddf8775a112bbda
Mike Cornelison <ralphie655@gmail.com>
2020-05-11 14:19:55 -0500

    Show message for incorrect answer

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -15,13 +15,29 @@ class QuizQuestion extends Component
             })}
           </ul>
         </section>
+        { this.state.incorrectAnswer 
+            ? <p className='error'>Sorry, that's not right</p>
+            : null
+        }
       </main>
+    }
+
+    constructor(props)
+    {
+        super(props);
+
+        this.state = {incorrectAnswer: false};
     }
 
     handleClick(buttonText) {
         if (buttonText === this.props.quiz_question.answer)
         {
+            this.setState({incorrectAnswer: false});
             this.props.showNextQuestionHandler();
+        }
+        else
+        {
+            this.setState({incorrectAnswer: true});
         }
     }
 }
